fc9bca9d82f3947545cd412d4303bf9226488a8f
Ruth Ahoueya <ruahoueya@gmailcom>
2025-06-23 16:57:05 +0100

    feat: rename principal page

--- fichier.js
+++ fichier.js
@@ -6,25 +6,25 @@ if(bouton){
     })
 }
 
-// const pourcentage = document.querySelector('.percent');
-// const progressTag = document.querySelector('.progress');
-// let count = 0;
-// const percent = 20;
-// const iteration = 5;
-// const intervalTime = 500;
-// function animate() {
-//     if (count < iteration) {
-//         count++;
-//         const occurrentPercent = count * percent;
-//         progressTag.style.width = (count * 200) + 'px';
-//         progressTag.style.backgroundColor = "white";
-//         pourcentage.textContent = occurrentPercent + '%';
-//     } else {
-//         clearInterval(loading);
-//         // window.location.href = "http://127.0.0.1:5500/staffex.html"; 
-//     }
-// }
-// const loading = setInterval(animate, intervalTime);
+const pourcentage = document.querySelector('.percent');
+const progressTag = document.querySelector('.progress');
+let count = 0;
+const percent = 20;
+const iteration = 5;
+const intervalTime = 500;
+function animate() {
+    if (count < iteration) {
+        count++;
+        const occurrentPercent = count * percent;
+        progressTag.style.width = (count * 200) + 'px';
+        progressTag.style.backgroundColor = "white";
+        pourcentage.textContent = occurrentPercent + '%';
+    } else {
+        clearInterval(loading);
+        // window.location.href = "http://127.0.0.1:5500/staffex.html"; 
+    }
+}
+const loading = setInterval(animate, intervalTime);
 
 const menu = document.querySelector('#bouton');
 if(menu){
